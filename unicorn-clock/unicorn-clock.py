8e2f94081232f988335e7542201be6485a67a8c9
vitroid <vitroid@gmail.com>
2022-06-19 15:18:32 +0900

    .

--- unicorn-clock/unicorn-clock.py
+++ unicorn-clock/unicorn-clock.py
@@ -1,10 +1,8 @@
 from PIL import ImageFont, ImageDraw
 import unicornhathd
 from PIL import Image
-import numpy as np
 import time
-# from datetime import datetime
-# import pytz
+import colorsys
 
 class DisplayOutput():
     def __init__(self):
@@ -24,7 +22,10 @@ class DisplayOutput():
 
         self.hat.show()
 
-def clock(now):
+def hsv2rgb(h,s,v):
+    return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h,s,v))
+
+def clockface(now):
     img = Image.new("RGB", (16,16))
     draw = ImageDraw.Draw(img)
 
@@ -44,28 +45,31 @@ def clock(now):
         y = 0
     img.putpixel((x,y), (255,255,255))
 
+    hue = (time.time() / 60) % 1.0
 
     # hour
-    font = ImageFont.truetype("/home/pi/github/raspberry/unicorn-clock/MyriadPro-Regular.otf", 12)
+    font = ImageFont.truetype("/home/pi/github/raspberry/unicorn-clock/MyriadPro-Regular.otf", 10)
     msg = f"{now.tm_hour:02d}"
     w, h = draw.textsize(msg, font=font)
-    draw.text((0, -1), msg, font=font, fill=(255,64,64))
+    # draw.text((0, -1), msg, font=font, fill=(255,64,64))
+    draw.text((0, -1), msg, font=font, fill=hsv2rgb(hue, 0.0, 1.0))
 
     # minute
     font = ImageFont.truetype("/home/pi/github/raspberry/unicorn-clock/MyriadPro-Regular.otf", 14)
     msg = f"{now.tm_min:02d}"
     w, h = draw.textsize(msg, font=font)
-    draw.text((16-w, 16-h), msg, font=font, fill=(255,128,128), stroke_width=1, stroke_fill=(0,0,0))
+    # draw.text((16-w, 16-h), msg, font=font, fill=(255,128,128), stroke_width=1, stroke_fill=(0,0,0))
+    draw.text((16-w, 16-h), msg, font=font, fill=hsv2rgb(hue, 0.8, 1.0), stroke_width=1, stroke_fill=(0,0,0))
 
     return img
 
 # timezone = pytz.timezone('Asia/Tokyo')
 output = DisplayOutput()
+last = time.localtime()
 while True:
     now = time.localtime()
-    # print(now)
-    # now = datetime.now(timezone)
-    # print(now)
-    img = clock(now)
-    output.write(img)
-    time.sleep(0.2)
+    if now.tm_sec != last.tm_sec:
+        img = clockface(now)
+        output.write(img)
+    time.sleep(0.1)
+    last = now
